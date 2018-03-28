aaa8775b43fcdba21d77be14432c6ff3a45e8b74
m4rc0z <m.zirkenbach@gmx.de>
2018-03-28 22:21:59 +0200

    adapt ratingcomponent to update rating if the props will update

--- app/components/ratingComponent/ratingComponent.js
+++ app/components/ratingComponent/ratingComponent.js
@@ -1,21 +1,39 @@
 import {Rating} from 'react-native-elements';
 import React from 'react';
+import {View} from "react-native";
 
 class RatingComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            rating: this.props.ratingValue
+            rating: this.props.ratingvalue
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (this.props.ratingvalue !== nextProps.ratingvalue) {
+            this.setState({
+                rating: nextProps.ratingvalue,
+            });
         }
     }
 
+    componentWillUpdate(nextProps, nextState) {
+        console.log("" + this.ratingElement);
+        this.ratingElement.setCurrentRating(nextState.rating);
+    }
+
+
     render() {
         return (
-            <Rating
-                imageSize={20}
-                readonly
-                startingValue={this.props.ratingValue}
-            />
+            <View>
+                <Rating
+                    ref={(ref) => {this.ratingElement = ref}}
+                    imageSize={20}
+                    readonly
+                    startingValue={this.state.rating}
+                />
+            </View>
         )
     }
 }
