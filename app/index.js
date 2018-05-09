7e49496624f6ab149db25b308079524cfacb9a73
m4rc0z <m.zirkenbach@gmx.de>
2018-05-09 23:46:50 +0200

    include navigationservice to set the reference to the navigator of the app

--- app/index.js
+++ app/index.js
@@ -3,16 +3,34 @@ import {StyleSheet, Text, View} from 'react-native';
 import HeaderComponent from "./pages/header/header";
 import MainPartComponent from "./pages/mainpart/mainpart";
 import Loader from "./components/loader/loader";
-import {createStackNavigator} from 'react-navigation';
+import {createBottomTabNavigator, createStackNavigator} from 'react-navigation';
+import SearchComponent from "./components/searchComponent/searchComponent";
+import NavigationService from "./services/navigation/NavigationService";
 
-const App = createStackNavigator({
+const TopLevelNavigator = createStackNavigator({
     SplashScreen: { screen: Loader },
-    MainNavigator: { screen: MainPartComponent }
+    MainNavigator: { screen: MainPartComponent },
+    Search: { screen: createBottomTabNavigator({
+            SearchBar: { screen: SearchComponent },
+            Home: { screen: MainPartComponent },
+})},
 }, {
     headerMode: 'screen',
+    cardStyle: { backgroundColor: 'white'},
     navigationOptions: {
-        header: <HeaderComponent/>
+        header: <HeaderComponent />
     }
 });
+
+class App extends React.Component {
+    render() {
+        return (
+            <TopLevelNavigator
+                ref={navigatorRef => {
+                    NavigationService.setTopLevelNavigator(navigatorRef)
+                }}/>
+        )
+    };
+}
 
 export default App;
